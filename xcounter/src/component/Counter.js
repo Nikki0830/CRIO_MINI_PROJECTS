b057247d6c9fa8622b897b283ab8d404e6c8b656
Nikki0830 <gniki0830@gmail.com>
2025-03-08 12:07:14 +0530

    xcounter

---
+++ xcounter/src/component/Counter.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import "./Counter.css";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <div
+      className="counter_div"
+      style={{ textAlign: "center", marginTop: "20px" }}
+    >
+      <h1>Counter App</h1>
+      <h4>Count {count}</h4>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+    </div>
+  );
+}
+
+export default Counter;
