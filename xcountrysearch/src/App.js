6263240eb8bb46e6b2e7843fee1f8e65a57e4a0c
Nikki0830 <gniki0830@gmail.com>
2025-03-10 19:19:05 +0530

    xcoutriessearch

---
+++ xcountrysearch/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Xcountrysearch from './component/Xcountrysearch';
+
+function App() {
+  return (
+    <div className="App">
+ <Xcountrysearch/>
+    </div>
+  );
+}
+
+export default App;
---
+++ xcountrysearch/src/component/Xcountrysearch.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./Xcountrysearch.css";
+import { useEffect, useState } from "react";
+
+function Country() {
+  const [countries, setCountries] = useState([]);
+  const [searchTerm, setsearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch(
+      "https://countries-search-data-prod-812920491762.asia-south1.run.app/countries"
+    )
+      .then((response) => response.json())
+      .then((json) => setCountries(json))
+
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+  //   console.log(countries);
+
+  const filteredcountries = countries.filter((country) =>
+    country.common.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  return (
+    <div className="main_div">
+      {/*input div */}
+      <input
+        type="text"
+        placeholder="Search countries..."
+        value={searchTerm}
+        onChange={(e) => setsearchTerm(e.target.value)}
+      />
+      <div className="countries_div">
+        {filteredcountries.map(({ common, png }) => (
+          <div key={common} className="countryCard">
+            {" "}
+            {/* Changed from flag_img_div */}
+            <img src={png} alt="flag_images" className="flag_img" />
+            <h3 className="flag_name">{common}</h3>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Country;
